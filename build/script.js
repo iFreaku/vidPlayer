bc0c4bb8d63fef1d159f8d3837f34dc8baec14fa
iFreaku <90742387+iFreaku@users.noreply.github.com>
2024-06-27 12:01:03 +0530

    Update

--- build/script.js
+++ build/script.js
@@ -137,24 +137,26 @@ setInterval(getCurrentImage, 100);
 
 
 window.onload = function() {
-  // Get the modal
   var modal = document.getElementById("myModal");
-
-  // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
-  // Display the modal when the page is fully loaded
-  modal.style.display = "block";
+  setTimeout(function() {
+      modal.classList.add("show");
+  }, 0);
 
-  // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
-      modal.style.display = "none";
+      modal.classList.remove("show");
+      setTimeout(function() {
+          modal.style.display = "none";
+      }, 500);
   }
 
-  // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
       if (event.target == modal) {
-          modal.style.display = "none";
+          modal.classList.remove("show");
+          setTimeout(function() {
+              modal.style.display = "none";
+          }, 500);
       }
   }
 }
